0c83cd231f1c93c40d15f38991249a3b6c848fa6
NS-natsu <104343731+NS-natsu@users.noreply.github.com>
2024-09-28 15:56:04 +0900

    Delete screen directory

--- screen/background/background.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-type b1 && type b2 && type b3 && type b4 && type b5 && ^
-type b6 && type b7 && type b8 && type b9 && type b10 && ^
-type b11 && type b12 && type b13 && type b14 && type b15 && ^
-type b16 && type b17 && type b18 && type b19 && type b20 && ^
-type b21 && type b22 && type b23
-@echo on
--- screen/mino/binary/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-type start.bin
-type %1
-type end.bin
-@echo on
